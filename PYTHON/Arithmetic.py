ae586fb829fe4032f00b7e4230cea22e74e0e96e
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-11-18 21:31:43 -0200

    strings

--- PYTHON/Arithmetic.py
+++ PYTHON/Arithmetic.py
@@ -1,7 +1,3 @@
-import random
-import sys
-import os
-
 first_number = 5
 second_number = 2
 print(f"{first_number} + {second_number} = {first_number + second_number}")
---
+++ PYTHON/Strings.py
@@ -0,0 +1,10 @@
+import random
+import sys
+import os
+
+quote = "Always remember you are unique"
+
+multi_line_quote = "Just like everyone else"
+
+new = quote + " " + multi_line_quote
+print(new)
