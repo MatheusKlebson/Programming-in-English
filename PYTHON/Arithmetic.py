00f34b6271cbe4983fc41031d4aec91d2d6d1cbc
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-11-25 11:49:49 -0200

    Arithmetic

--- PYTHON/Arithmetic.py
+++ PYTHON/Arithmetic.py
@@ -1,5 +1,5 @@
-first_number = 5
-second_number = 2
+first_number = int(input("Number First: "))
+second_number = int(input("Number Second: "))
 print(f"{first_number} + {second_number} = {first_number + second_number}")
 print(f"{first_number} - {second_number} = {first_number - second_number}")
 print(f"{first_number} / {second_number} = {first_number / second_number}")
